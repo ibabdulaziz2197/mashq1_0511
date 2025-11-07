cad2b89817c880af8badc28df28d8e1762d00d2e
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2025-11-07 17:09:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# 1
+kop = 1
+for i in range(1, 6):
+    kop *= i
+
+print(kop)
+
+
+# 2
+kop = 1
+for i in range(1, 11):
+    if i % 2 == 0:
+        kop *= i
+
+print(kop)
+
+# 4
+kop = 1
+for i in range(3, 9):
+    kop *= i
+
+print(kop)
+
+5
+kop = 1
+for i in range(1,7):
+    if i % 1 == 0:
+        kop *= i
+
+print(kop)
